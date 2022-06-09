0ff287aba5c49929c94edcf829860932646e9b75
cflv5 <bedirhanakcay@icloud.com>
2022-06-09 23:26:47 +0300

    feat: add status field to the base exception

--- src/main/java/tr/edu/yildiz/ce/se/base/exception/SeBaseException.java
+++ src/main/java/tr/edu/yildiz/ce/se/base/exception/SeBaseException.java
@@ -1,6 +1,9 @@
 package tr.edu.yildiz.ce.se.base.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class SeBaseException extends RuntimeException {
+    private HttpStatus status;
 
     public SeBaseException() {
     }
@@ -8,5 +11,14 @@ public class SeBaseException extends RuntimeException {
     public SeBaseException(String message) {
         super(message);
     }
-    
+
+    public SeBaseException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
 }
