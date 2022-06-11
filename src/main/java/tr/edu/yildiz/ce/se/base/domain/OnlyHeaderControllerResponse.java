753f6524052294dc1be9a9e9284a08a71cac4311
cflv5 <bedirhanakcay@icloud.com>
2022-06-11 12:34:37 +0300

    feat: create base response class includes only response header

---
+++ src/main/java/tr/edu/yildiz/ce/se/base/domain/OnlyHeaderControllerResponse.java
@@ -0,0 +1,24 @@
+package tr.edu.yildiz.ce.se.base.domain;
+
+import java.io.Serializable;
+
+public class OnlyHeaderControllerResponse implements Serializable {
+    private final ResponseHeader responseHeader;
+
+    public OnlyHeaderControllerResponse(ResponseHeader responseHeader) {
+        this.responseHeader = responseHeader;
+    }
+
+    public static OnlyHeaderControllerResponse success() {
+        return new OnlyHeaderControllerResponse(ResponseHeader.success());
+    }
+
+    public static OnlyHeaderControllerResponse failed(HeaderMessage message) {
+        return new OnlyHeaderControllerResponse(ResponseHeader.fail(message));
+    }
+
+    public ResponseHeader getResponseHeader() {
+        return responseHeader;
+    }
+
+}
