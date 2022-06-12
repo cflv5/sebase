fdb59ef098c6192b7ab4e2a7e516b0fb1ed3fd1f
cflv5 <bedirhanakcay@icloud.com>
2022-06-12 20:53:16 +0300

    feat: create NamedResource class

---
+++ src/main/java/tr/edu/yildiz/ce/se/base/domain/io/NamedResource.java
@@ -0,0 +1,42 @@
+package tr.edu.yildiz.ce.se.base.domain.io;
+
+import org.springframework.core.io.ByteArrayResource;
+
+public class NamedResource extends ByteArrayResource {
+    private final String name;
+
+    public NamedResource(byte[] byteArray, String name) {
+        super(byteArray);
+        this.name = name;
+    }
+
+    public String getResourceName() {
+        return name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        NamedResource other = (NamedResource) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+}
