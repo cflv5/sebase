710ef455079e37824c6a57b35123c89b1e70ea43
cflv5 <bedirhanakcay@icloud.com>
2022-06-12 22:57:04 +0300

    feat: handle exception occurs when making rest call

--- src/main/java/tr/edu/yildiz/ce/se/base/service/SeRestService.java
+++ src/main/java/tr/edu/yildiz/ce/se/base/service/SeRestService.java
@@ -2,8 +2,6 @@ package tr.edu.yildiz.ce.se.base.service;
 
 import org.springframework.http.HttpMethod;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 public interface SeRestService {
-    <T, S> T call(String url, S payload, HttpMethod method, Class<T> clazz) throws JsonProcessingException;
+    <T, S> T call(String url, S payload, HttpMethod method, Class<T> clazz);
 }
--- src/main/java/tr/edu/yildiz/ce/se/base/service/StandartSeRestService.java
+++ src/main/java/tr/edu/yildiz/ce/se/base/service/StandartSeRestService.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -16,13 +18,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 import tr.edu.yildiz.ce.se.base.context.TenantContext;
 import tr.edu.yildiz.ce.se.base.exception.SeBaseException;
 
 @Service
 public class StandartSeRestService implements SeRestService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StandartSeRestService.class);
+
     private final RestTemplate restTemplate;
 
     @Value("${se.base.rest-client.access-token}")
@@ -33,17 +35,22 @@ public class StandartSeRestService implements SeRestService {
     }
 
     @Override
-    public <T, S> T call(String url, S payload, HttpMethod method, Class<T> clazz) throws JsonProcessingException {
+    public <T, S> T call(String url, S payload, HttpMethod method, Class<T> clazz) {
         var headers = Map.ofEntries(
                 entry(ACCESS_TOKEN, List.of(accessToken)),
                 entry(TENANT_ID, List.of(TenantContext.getCurrentTenant().getTenantId())));
+        try {
+            HttpEntity<S> request = new HttpEntity<>(payload, CollectionUtils.toMultiValueMap(headers));
+            var response = Optional.ofNullable(restTemplate.exchange(url, method, request, clazz).getBody());
+            return response
+                    .orElseThrow(
+                            () -> new SeBaseException("Could not make a REST call", HttpStatus.INTERNAL_SERVER_ERROR));
 
-        HttpEntity<S> request = new HttpEntity<>(payload, CollectionUtils.toMultiValueMap(headers));
+        } catch (Exception e) {
+            LOGGER.error("Exception occured while making rest call", e);
+            throw new SeBaseException("Could not make a REST call", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
-        var response = Optional.ofNullable(restTemplate.exchange(url, method, request, clazz).getBody());
-
-        return response
-                .orElseThrow(() -> new SeBaseException("Could not make a REST call", HttpStatus.INTERNAL_SERVER_ERROR));
     }
 
 }
